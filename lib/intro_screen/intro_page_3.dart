d19aa6489421b0e3976430cdeadf8c2fb1fe5f63
Pham Anh Quang <104234748+anh-quang@users.noreply.github.com>
2022-11-01 20:27:28 +0700

    nhanh 1

---
+++ lib/intro_screen/intro_page_3.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IntroPage3 extends StatelessWidget {
+  const IntroPage3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text('Page 3')),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:ecommerce_app/onboarding_screen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,  
+      home: const OnBoardingScreen(),
+    );
+  }
+}
